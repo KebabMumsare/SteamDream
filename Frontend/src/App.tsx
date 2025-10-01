8235174bf90887c0c1ac3cb8ef39875bd30a4a95
Finn-S123 <Finn.skattum@elev.ga.ntig.se>
2025-10-01 14:14:43 +0200

    merge fix

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,21 +1,32 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import './App.css'
 
 import Card from './components/Card';
+import Navbar from './components/Navbar';
+import Profile from './Profile';
 
 function App() {
 
   return (
-    <>
-      <h1 className="text-red-500 underline">
-        Hello world!
-      </h1>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+            </>
+          }
+        />
+      </Routes>
+      <Routes>
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
