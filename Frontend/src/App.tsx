ba73a1ed30a0f9c4cf6c5e19480764a55b368c46
Jesper Ahlström <143940213+KebabMumsare@users.noreply.github.com>
2025-10-01 09:22:38 +0200

    routes implemented

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,20 +1,34 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-import Card from './components/Card';
-import Navbar from './components/Navbar';
+import Card from "./components/Card";
+import Navbar from "./components/Navbar";
+import Profile from "./Profile";
 
 function App() {
-
   return (
-    <>
-    
+    <BrowserRouter>
       <Navbar />
-      <h1 className="text-red-500 underline">
-        Hello world!
-      </h1>
-      <Card />
-    </>
-  )
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <h1 className="text-red-500 underline">
+                <Link to="/profile">Profile</Link>
+              </h1>
+              <h1 className="text-red-500 underline">Hello world!</h1>
+              <Card />
+            </>
+          }
+        />
+      </Routes>
+      <Routes>
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+      
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
