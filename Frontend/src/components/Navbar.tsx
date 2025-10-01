665c07f7afba9788fa076dc0705fc4b170caea39
isakcarlsson25 <isak.carlsson@elev.ntig.se>
2025-10-01 09:23:21 +0200

    Added icons

--- Frontend/src/components/Navbar.tsx
+++ Frontend/src/components/Navbar.tsx
@@ -1,21 +1,26 @@
-
-
 function Navbar() {
   return (
     <nav
-      className="fixed top-0 left-0 right-0 z-50 mx-10 mt-10 py-6 px-8 shadow flex items-center justify-between text-white rounded-full"
+      className="fixed top-0 left-0 right-0 z-50 mt-10 py-5 px-8 shadow flex items-center justify-between text-white rounded-full w-[90vw] mx-auto"
       style={{ backgroundColor: '#1B2838' }}
     >
       <span className="font-bold text-xl tracking-wide rounded-full px-4 py-2" style={{ backgroundColor: '#1B2838' }}>
         SteamDream
       </span>
       <div className="flex-1 flex justify-center">
-        <input
-          type="text"
-          placeholder="Search..."
-          className="w-[32rem] px-16 py-4 rounded-3xl text-white placeholder-white placeholder:text-xl focus:outline-none focus:ring-2 focus:ring-blue-400"
-          style={{ backgroundColor: '#66C0F4' }}
-        />
+        <div className="relative w-[32rem]">
+          <img
+            src="/src/components/Icons/Search.png"
+            alt="Search"
+            className="absolute left-5 top-3/4 -translate-y-1/2 w-8 h-8"
+          />
+          <input
+            type="text"
+            placeholder="Search..."
+            className="w-full pl-16 pr-6 py-4 rounded-3xl text-white placeholder-white placeholder:text-xl focus:outline-none focus:ring-2 focus:ring-blue-400"
+            style={{ backgroundColor: '#66C0F4' }}
+          />
+        </div>
       </div>
       <ul className="flex gap-8 items-center">
         <li>
